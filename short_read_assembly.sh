3b3093c85dfa2fa0cb3f02cc969808d7d674d0f1
Aylward Lab <frank.o.aylward@gmail.com>
2018-04-16 21:08:29 -0400

    Add files via upload

--- short_read_assembly.sh
+++ short_read_assembly.sh
@@ -21,3 +21,21 @@ fastq-dump -X 10000 --split-3 SRR6764339
 spades.py -1 SRR6764339_1.fastq -2 SRR6764339_2.fastq -o phage -t 4 -k 21 &> log.txt
 
 
+
+# now after assembly we can map reads and get a coverage estimate. 
+# first we can get the longest contig and index it so we can map reads. 
+samtools faidx phage/contigs.fasta NODE_1_length_141366_cov_30.585153 > phage_contig.fasta
+bowtie2-build phage_contig.fasta phage_bowtie_db
+
+# now we can map reads against the reference using bowtie2
+bowtie2 -1 SRR6764339_1.fastq -2 SRR6764339_2.fastq -x phage_bowtie_db -S mapping_output.SAM
+
+# after that we need to process the .sam file by converting it into a .bam file, and then sorting and indexing it before we can get read mapping statistics. 
+samtools view -bS -F 4 mapping_output.SAM > mapping_output.bam
+samtools sort mapping_output.bam mapping_output.sort
+samtools index mapping_output.sort.bam
+samtools idxstats mapping_output.sort.bam
+
+# Now that we have our indexed .bam file, we can use bedtools to get a coverage estimate. I will pipe the output and cut it to retain only the third column, which will contain the coverage estimates for each bp of the genome. 
+bedtools genomecov -d -g phage_contig.fasta -ibam mapping_output.sort.bam | cut -f 3 > phage_coverage.txt
+
